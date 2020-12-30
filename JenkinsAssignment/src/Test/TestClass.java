bebfd444bbfa986a6cdec1bc3c2015fcc961e5c3
aaryn <L00088572@student.lyit.ie>
2020-12-30 16:30:19 +0000

    Added another print statement

--- JenkinsAssignment/src/Test/TestClass.java
+++ JenkinsAssignment/src/Test/TestClass.java
@@ -6,7 +6,8 @@ public class TestClass
 	public static void main(String[] args) 
 	{
 		System.out.print("Hello ");
-		System.out.print("World");
+		System.out.print("World ");
+		System.out.print("Again");
 	}
 
 }
