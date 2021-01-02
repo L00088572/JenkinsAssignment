8fa7143bb76004ca5fca2ecc801d775b48afaeb5
aaryn <L00088572@student.lyit.ie>
2021-01-02 00:03:00 +0000

    Added Jenkinsfile and a new method that adds 2 numbers to Test class

--- JenkinsAssignment/src/Test/TestClass.java
+++ JenkinsAssignment/src/Test/TestClass.java
@@ -2,12 +2,17 @@ package Test;
 
 public class TestClass 
 {
-
+	public static int AddTwoNumbers(int num1, int num2)
+	{
+		return num1 + num2;
+	}
+	
 	public static void main(String[] args) 
 	{
 		System.out.print("Hello ");
-		System.out.print("World ");
-		System.out.print("Again");
+		System.out.println("World!");
+		System.out.print(AddTwoNumbers(10,5));
 	}
-
 }
+
+
